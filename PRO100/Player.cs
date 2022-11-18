425b2d8aa6dff946c7aa197c46c1f0cafe8670ec
enicewanner <enicewanner@student.neumont.edu>
2022-11-17 22:15:09 -0700

    Added variables player class

--- PRO100/Player.cs
+++ PRO100/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace PRO100
 {
     internal class Player
     {
+        int incomingDamage = 0;
+        int outgoingDamage = 0;
+
+        ArrayList playerDeck = new ArrayList();
+
     }
 }
